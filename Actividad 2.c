d3d0e4163e51e0aa39b38a50fd0170078849c684
1010Gabriel0101 <148381895+1010Gabriel0101@users.noreply.github.com>
2023-10-19 17:10:30 -0600

    Create Actividad 2.c

---
+++ Actividad 2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int opcion = 0;
+while( opcion != 5)
+{
+  printf("Introduce una opcion\n");
+  printf("Opcion 1: Torres de Hanoi\n");
+  printf("Opcion 2: Lobo,oveja y maiz\n");
+  printf("Opcion 3: Antro\n");
+  printf("Opcion 4: Impuestos\n");
+  printf("Opcion 5: Salir\n");
+  printf("\n\n\n");
+  scanf("%d",&opcion);
+  if(opcion == 1){
+  //Codigo de Hanoi
+  }
+  if(opcion == 2){
+  //Lobo, oveja y maiz
+  }
+  if(opcion == 3){
+  //Antro
+  }
+  if(opcion == 4){
+  //Impuestos
+  }
+  }
+printf("Salio del ciclo");
+}
