6a577d22b0ea8dc752470126e1de58e58edd2aea
1010Gabriel0101 <148381895+1010Gabriel0101@users.noreply.github.com>
2023-10-19 18:07:17 -0600

    Create Actividad 4.c

---
+++ Actividad 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int cont = 10;
+  while(cont >= 1)
+    {
+      cont -= 1; //cont = cont -1;
+      if(cont == 4)
+      {
+        break;
+      }
+      printf("%d\n",cont);
+    }
+}
